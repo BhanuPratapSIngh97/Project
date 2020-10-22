65f7d891f67a6b9d0466fe6262e087acacdb988d
BhanuPratapSIngh97 <48156269+BhanuPratapSIngh97@users.noreply.github.com>
2020-10-22 18:25:25 +0530

    Delete new.js
    nknl

--- new.js
+++
@@ -1,8 +0,0 @@
-exports.testing = (req,res)=>{
-    console.log(req);
-   res.send('we are using next function, no is'+ req.params.id);
-}
-
-exports.test = (req,res,next) =>{
-    res.send("okk node is working");
-}
