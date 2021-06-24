38175a965020cf5da29713b7cf735b11c4900657
BhanuPratapSIngh97 <48156269+BhanuPratapSIngh97@users.noreply.github.com>
2021-06-24 13:07:35 +0530

    Delete controllers directory

--- controllers/abc.js
+++
@@ -1,6 +0,0 @@
-exports.testing = (req,res,next)=>{
-    if(req.params.id <= 5 ){
-        res.send('no is less then 5');
-    }
-    next();
-}
--- controllers/new.js
+++
@@ -1,4 +0,0 @@
-exports.testing = (req,res)=>{
-    console.log(req);
-   res.send('we are using next function, no is'+ req.params.id);
-}
